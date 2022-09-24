46ebea007ef8f7585a11fa2745f593cb6d98e0cb
imanhle97 <quocnotanh@gmail.com>
2022-09-24 20:53:36 +0700

    Updates

---
+++ precache-manifest.84ec509b257c6f47dc0c6660940c16ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b26cc897a04538bef67d",
+    "url": "/chanqu/static/css/main.5a922e9b.chunk.css"
+  },
+  {
+    "revision": "b26cc897a04538bef67d",
+    "url": "/chanqu/static/js/main.433483ee.chunk.js"
+  },
+  {
+    "revision": "11ebba2ff4c23669e4bd",
+    "url": "/chanqu/static/js/runtime~main.fbd8c86f.js"
+  },
+  {
+    "revision": "7bfa2a65a603a0732b75",
+    "url": "/chanqu/static/js/2.41e0e4aa.chunk.js"
+  },
+  {
+    "revision": "bf10dffb79047bea8bd5589468e0640f",
+    "url": "/chanqu/index.html"
+  }
+];
